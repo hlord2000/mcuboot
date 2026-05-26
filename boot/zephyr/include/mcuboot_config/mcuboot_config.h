f48258fa5ae458a01da51505450626537e6c9d78
hlord2000 <kellyhlord@gmail.com>
2026-05-26 13:41:12 -0400

    boot: zephyr: expose delta DFU restore configuration
    Signed-off-by: hlord2000 <kellyhlord@gmail.com>

--- boot/zephyr/include/mcuboot_config/mcuboot_config.h
+++ boot/zephyr/include/mcuboot_config/mcuboot_config.h
@@ -199,6 +199,14 @@
 #define MCUBOOT_DECOMPRESS_IMAGES
 #endif
 
+#ifdef CONFIG_BOOT_DELTA_DFU
+#define MCUBOOT_DELTA_DFU
+#define MCUBOOT_DELTA_SECTOR_BUF_SIZE CONFIG_BOOT_DELTA_DFU_SECTOR_BUFFER_SIZE
+#ifdef CONFIG_BOOT_DELTA_DFU_RESTORE
+#define MCUBOOT_DELTA_DFU_RESTORE
+#endif
+#endif
+
 /* Invoke hashing functions directly on storage device. This requires the device
  * be able to map storage to address space or RAM.
  */
